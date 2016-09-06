e0015db1f0e68b7180ca71f4e4f8bb61b7cc44ac
sophiabond <sophiabond00@gmail.com>
2016-09-06 09:19:07 -0700

    Added Files

---
+++ APLesson_02/APLesson_02.java
@@ -0,0 +1,8 @@
+public class APLesson_02
+{
+	public static void main(String[]args)
+	{
+		String name = "APCompsci";
+		System.out.println(name);
+	}
+}
---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,10 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int num1 = 10;
+		int num2 = 5;
+		
+		System.out.println(num1 * num2);
+	}
+}
